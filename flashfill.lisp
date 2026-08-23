9c5b30e0b95afb77dad3f92f4fd03356046440bd
bitorhugo <vhsoo@proton.me>
2026-08-23 05:40:30 +0100

    feat: add observational-equivalence pruning

--- flashfill.lisp
+++ flashfill.lisp
@@ -10,6 +10,11 @@
 (defun concat (&rest parts)
   (apply #'concatenate 'string parts))
 
+;; Note:
+;; The above are called grammar compositions, and together they form a grammar (DSL).
+;; Every program written is some compostion of these, nothing else.
+;; This restriction is what makes search over programs tracktable (easy to follow) later.
+
 (defun eval-prog (form s)
   (let ((op (first form)))
     (cond ((eq 'sub-str op)
@@ -40,17 +45,37 @@
 	(push `(concat ,(nth i p) ,(nth j p))
 	      result)))))
 
-(defun all-depth-1-programs (s)
+(defun prune-equivalent (list-of-programs s)
+  (let ((output->programs (make-hash-table :test 'equal)))
+    (dolist (prog list-of-programs)
+      (let ((output (eval-prog prog s)))
+	(setf (gethash output output->programs)
+	      prog)))
+    (hash-table-values output->programs)))
+
+(defun concat-and-prune (p s)
+  (let ((output->programs (make-hash-table :test 'equal)))
+    (dotimes (i (length p))
+      (dotimes (j (length p))
+	(let* ((program `(concat ,(nth i p) ,(nth j p)))
+	       (output (eval-prog program s)))
+	  (setf (gethash output output->programs)
+		program))))
+    (hash-table-values output->programs)))
+
+(defun all-depth-1-programs (s out)
   (append (all-sub-str-programs (length s))
-	  (all-literal-programs s)))
+	  (all-literal-programs s)
+	  (all-literal-programs out)))
 
-(defun all-depth-2-programs (s)
-  (let ((d1p (all-depth-1-programs s)))
-    (append d1p (all-concat-programs d1p))))
+(defun all-depth-2-programs (s out)
+  (let ((d1p (all-depth-1-programs s out)))
+    (prune-equivalent (append d1p (all-concat-programs d1p))
+		      s)))
 
-(defun all-depth-3-programs (s)
-  (let ((d2p (all-depth-2-programs s)))
-    (append d2p (all-concat-programs d2p)))) ; at this point we have 18M programs.. Intractable
+(defun all-depth-3-programs (s out)
+  (let ((d2p (all-depth-2-programs s out)))
+    (append d2p (concat-and-prune d2p s))))
 
 (defun %filter-correct (list-of-programs input expected-output)
   (remove-if-not (lambda (program) (equal (eval-prog program input)
@@ -65,51 +90,58 @@
 			  input-output-pairs))
 		 list-of-programs))
 
-;; Note:
-;; The above are called grammar compositions, and together they form a grammar (DSL).
-;; Every program written is some compostion of these, nothing else.
-;; This restriction is what makes search over programs tracktable (easy to follow) later.
-
 
 ;; tests
 
-(let ((name "John Smith"))
-  ;; John
-  (format t "~a~%" (sub-str "John Smith" 0 4))
+(defun run-tests ()
 
-  ;; J. Smith -> J dot space Smith
-  (format t "~a~%" (concat (sub-str "John Smith" 0 1)
-			   (literal ".")
-			   (literal " ") ; we could use sub-str starting from 4 as well
-			   (sub-str "John Smith" 5))))
+  (let ((name "John Smith"))
+    ;; John
+    (format t "~a~%" (sub-str "John Smith" 0 4))
 
-(let ((name "Jane Doe"))
-  (format t "~a~%" (concat (sub-str name 0 1)
-			   (literal ".")
-			   (literal " ")
-			   (sub-str name 5))))
+    ;; J. Smith -> J dot space Smith
+    (format t "~a~%" (concat (sub-str "John Smith" 0 1)
+			     (literal ".")
+			     (literal " ") ; we could use sub-str starting from 4 as well
+			     (sub-str "John Smith" 5))))
 
-(format t "~a~%" (eval-prog '(sub-str 0 4) "John Smith"))
+  (let ((name "Jane Doe"))
+    (format t "~a~%" (concat (sub-str name 0 1)
+			     (literal ".")
+			     (literal " ")
+			     (sub-str name 5))))
 
-(format t "~a~%" (eval-prog '(literal "!") "John Smith"))
+  (format t "~a~%" (eval-prog '(sub-str 0 4) "John Smith"))
 
-(format t "~a~%" (eval-prog '(concat (sub-str 0 1) (literal ".") (literal " ") (sub-str 5))
-			    "John Smith"))
+  (format t "~a~%" (eval-prog '(literal "!") "John Smith"))
 
-(format t "~a~%" (all-sub-str-programs 4))
+  (format t "~a~%" (eval-prog '(concat (sub-str 0 1) (literal ".") (literal " ") (sub-str 5))
+			      "John Smith"))
 
-(print (all-literal-programs "ab"))
+  (format t "~a~%" (all-sub-str-programs 4))
 
-(print (all-depth-1-programs "ab"))
+  (print (all-literal-programs "ab"))
 
-(print (%filter-correct (all-depth-1-programs "ab") "ab" "a"))
-;; (mapcar (rcurry #'eval-prog "ab") '((SUB-STR 0 1) (LITERAL "a"))) = ("a" "a")
+  (print (all-depth-1-programs "ab" ""))
 
-(print (filter-correct (all-depth-1-programs "ab") '(("ab" . "a") ("ab" . "a"))))
+  (print (%filter-correct (all-depth-1-programs "ab" "") "ab" "a"))
+  ;; (mapcar (rcurry #'eval-prog "ab") '((SUB-STR 0 1) (LITERAL "a"))) = ("a" "a")
 
-(print (filter-correct (all-depth-1-programs "ab") '(("ab" . "a") ("ab" . "b"))))
+  (print (filter-correct (all-depth-1-programs "ab" "") '(("ab" . "a") ("ab" . "a"))))
 
-(print (length (all-concat-programs (all-depth-1-programs "ab"))))
+  (print (filter-correct (all-depth-1-programs "ab" "") '(("ab" . "a") ("ab" . "b"))))
 
-(format t "depth 1: ~a programs~%" (length (all-depth-1-programs "John Smith")))
-(format t "depth 2: ~a programs~%" (length (all-depth-2-programs "John Smith")))
+  (print (length (all-concat-programs (all-depth-1-programs "ab" ""))))
+
+  (format t "depth 1: ~a programs~%" (length (all-depth-1-programs "John Smith" "")))
+  (format t "depth 2: ~a programs~%" (length (all-depth-2-programs "John Smith" "")))
+
+  (format t "after prunning: ~a programs~%"
+	  (length (prune-equivalent (all-depth-1-programs "ab" "") "ab")))
+  (format t "depth 2 pruned: ~a programs~%" (length (all-depth-2-programs "John Smith" "")))
+  (format t "depth 3 pruned: ~a programs~%" (length (all-depth-3-programs "Jane" "")))
+
+  (print (filter-correct (all-depth-3-programs "Jane Doe" "J. Doe") '(("Jane Doe" . "J. Doe"))))
+  (print (filter-correct (all-depth-3-programs "V Hugo" "V. Hugo") '(("V Hugo" . "V. Hugo"))))
+
+  (print (program-size '(concat (literal "J") (concat (literal ".") (sub-str 4 8))))) )
--- util.lisp
+++ util.lisp
@@ -1,12 +1,39 @@
 (in-package :cl-flashfill)
 
-(defun curry (fn &rest args)
+(defun curry (function &rest args)
   #'(lambda (&rest more-args)
-      (apply fn (append args more-args))))
+      (apply function (append args more-args))))
 
-(defun rcurry (fn &rest args)
+(defun rcurry (function &rest args)
   #'(lambda (&rest more-args)
-      (apply fn (append more-args args))))
+      (apply function (append more-args args))))
 
 (defun rac (list)
   (car (last list)))
+
+(defun hash-table->list (hash-table)
+  (let ((result (list)))
+    (maphash (lambda (k v) (push (cons k v) result)) hash-table)
+    (values result)))
+
+(defun map-hash-keys (function hash-table)
+  (maphash (lambda (k v)
+	     (declare (ignore v))
+	     (funcall function k))
+	   hash-table))
+
+(defun map-hash-values (function hash-table)
+  (maphash (lambda (k v)
+	     (declare (ignore k))
+	     (funcall function v))
+	   hash-table))
+
+(defun hash-table-keys (hash-table)
+  (let ((result (list)))
+    (map-hash-keys (lambda (k) (push k result)) hash-table)
+    (values result)))
+
+(defun hash-table-values (hash-table)
+  (let ((result (list)))
+    (map-hash-values (lambda (v) (push v result)) hash-table)
+    (values result)))
